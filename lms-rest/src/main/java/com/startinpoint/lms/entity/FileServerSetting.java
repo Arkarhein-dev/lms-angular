9209d27744ab9b59e6a8946d8bf52d96cd4830df
Arkarhein-dev <hein90510@gmail.com>
2026-09-08 16:58:19 +0630

    impl admin setting

---
+++ lms-rest/src/main/java/com/startinpoint/lms/entity/FileServerSetting.java
@@ -0,0 +1,4 @@
+package com.startinpoint.lms.entity;
+
+public class FileServerSetting {
+}
---
+++ lms-rest/src/main/java/com/startinpoint/lms/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,4 @@
+package com.startinpoint.lms.exception;
+
+public class ResourceAlreadyExistsException {
+}
